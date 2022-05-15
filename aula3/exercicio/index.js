b7193cedc4585f8fb424bdf2656be83f96e2d6aa
Eduardo Bueno <eduardo@ebssolucoescorporativas.com.br>
2022-05-15 11:03:41 -0300

    aula3

---
+++ aula3/exercicio/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Eduardo". Estou apendendo Javasript às', 10, 'da manhã.');
---
+++ aula3/index.js
@@ -0,0 +1,8 @@
+console.log('Aspas simples');
+console.log("Aspas duplas");
+console.log(`Crase`);
+console.log('Aspas "duplas"');
+console.log("Aspas 'simples'");
+console.log(`Crase com 'aspas simples' e "aspas duplas"`); // case usa o template engine
+
+console.log(typeof(35), typeof(15.85), typeof('Eduardo'));
